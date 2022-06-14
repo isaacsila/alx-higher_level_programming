04e82dc3d661038b7540fc200eb6f30697f22c28
isaacsila <isaacsila431@gmail.com>
2022-06-14 13:08:13 -0700

    correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-weight_average = __import__('100-weight_average').weight_average
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        return (sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
